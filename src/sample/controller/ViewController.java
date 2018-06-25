2c11257af5895d526690150d37a4b7cf618aacd1
Ioana <ioanaimbuzean@ymail.com>
2018-06-25 21:16:44 +0300

    Added view from db.

--- src/sample/controller/ViewController.java
+++ src/sample/controller/ViewController.java
@@ -2,9 +2,12 @@ package sample.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +17,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import sample.model.Student;
 
 public class ViewController {
 
@@ -25,22 +30,22 @@ public class ViewController {
     private URL location;
 
     @FXML
-    private TableView<?> viewTView;
+    private TableView<Student> viewTView;
 
     @FXML
-    private TableColumn<?, ?> viewPrenumeColumn;
+    private TableColumn<Student, String> viewPrenumeColumn;
 
     @FXML
-    private TableColumn<?, ?> viewNumeColumn;
+    private TableColumn<Student, String> viewNumeColumn;
 
     @FXML
-    private TableColumn<?, ?> viewMateriaColumn;
+    private TableColumn<Student, String> viewMateriaColumn;
 
     @FXML
     private Button viewExitButton;
 
     @FXML
-    private TableColumn<?, ?> viewVarstaColumn;
+    private TableColumn<Student, Integer> viewVarstaColumn;
 
     @FXML
     private Button viewBackButton;
@@ -48,6 +53,11 @@ public class ViewController {
 
     @FXML
     void initialize() {
+        viewNumeColumn.setCellValueFactory(new PropertyValueFactory<Student,String>("Nume"));
+        viewPrenumeColumn.setCellValueFactory(new PropertyValueFactory<Student,String>("Prenume"));
+        viewVarstaColumn.setCellValueFactory(new PropertyValueFactory<Student,Integer>("Varsta"));
+        viewMateriaColumn.setCellValueFactory(new PropertyValueFactory<Student,String>("Materia"));
+        viewTView.setItems(getStudents());
         viewBackButton.setOnAction(event -> {
             try {
                 backScreenButtonPushed(event);
@@ -67,4 +77,30 @@ public class ViewController {
         window.setScene(welcomeScene);
         window.show();
     }
+    public ObservableList<Student> getStudents() {
+        Student student;
+        String nume, prenume, materia;
+        int varsta;
+        ObservableList<Student> students = FXCollections.observableArrayList();
+        String dbUrl = "jdbc:mysql://localhost/admin?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+        String user = "root";
+        String password = "student";
+        try {
+            Connection myConn = DriverManager.getConnection(dbUrl, user, password);
+            Statement myStmt = myConn.createStatement();
+            ResultSet myRs = myStmt.executeQuery("select * from student");
+            while (myRs.next()) {
+                nume = myRs.getString(2);
+                prenume = myRs.getString(3);
+                varsta = Integer.parseInt(myRs.getString(4));
+                materia = myRs.getString(5);
+                System.out.println(nume+prenume+varsta+materia);
+                students.add(new Student(nume, prenume, varsta, materia));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
 }
--- src/sample/model/Student.java
+++ src/sample/model/Student.java
@@ -4,7 +4,7 @@ public class Student {
     private String nume, prenume, materia;
     private int varsta;
 
-    public Student(String nume, String prenume, String materia, int varsta) {
+    public Student(String nume, String prenume, int varsta, String materia) {
         this.nume = nume;
         this.prenume = prenume;
         this.materia = materia;
